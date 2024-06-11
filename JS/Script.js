f83e2f8c5e3058fe4b966eed16dccc2e0583f4c6
Thales Rangel <thalesran2006@gmail.com>
2024-06-10 23:09:52 -0300

    Add conteúdo
    - Adicionado o conteúdo das promoções diárias;
    - Melhorado um pouco da estilização da página;

--- JS/Script.js
+++ JS/Script.js
@@ -16,7 +16,16 @@ promosID = new Array("promo-Dom",
     "promo-Sex",
     "promo-Sab");
 
+promosDesc = new Array("Compre uma pizza grande e ganhe outra do mesmo sabor de graça! Escolha entre margherita, pepperoni ou vegetariana.",
+    "Peça nosso hambúrguer especial e ganhe uma bebida (refrigerante ou suco) de cortesia! Acompanhe com batatas fritas.",
+    "Compre dois sanduíches naturais e pague apenas pelo primeiro! Escolha entre frango, atum ou veggie.", 
+    "Compre uma latinha de Coca-Cola e ganhe um petisco à sua escolha! Acompanhe com nossos petiscos especiais. Uma combinação perfeita para a tarde.", 
+    "Compre uma pizza grande e ganhe uma bola de sorvete de sobremesa! Escolha seu sabor favorito.",
+    "Peça um pote de açaí e ganhe um energético para acompanhar! Perfeito para a sexta-feira agitada.", 
+    "Compre uma pizza grande e ganhe outra do mesmo sabor de graça! Escolha entre margherita, pepperoni ou vegetariana.");
+
 document.getElementById("day-promocao-area").style.backgroundImage = promosURL[date.getDay()];
+document.querySelector(".desc-promocao p").innerHTML = promosDesc[date.getDay()];
 document.getElementById(promosID[date.getDay()]).style.display = "flex";
 
 function selectCardapio(id, div){
